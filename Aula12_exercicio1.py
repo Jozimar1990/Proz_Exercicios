9edd03c7e64682d57cd43ffb86d81e941eefcd2f
Jozimar1990 <jozimar.1990@gmail.com>
2024-09-04 19:07:10 -0400

    linha 8: troca do 'in' pelo '=='

--- Aula12_exercicio1.py
+++ Aula12_exercicio1.py
@@ -5,4 +5,4 @@ Preparamos uma pergunta para você.
 Que animal é maior que 3 e menor que 4?
 ''')
 resposta = input().lower()
-print("Acertou!") if resposta in respostas[0] else print("Errou!")
+print("Acertou!") if resposta == respostas[0] else print("Errou!")

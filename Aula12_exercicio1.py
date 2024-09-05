cf4413893821a6136c05a0e9ea37677b73d4f5a1
Jozimar1990 <jozimar.1990@gmail.com>
2024-09-04 20:01:42 -0400

    novas atualizações

--- Aula12_exercicio1.py
+++ Aula12_exercicio1.py
@@ -1,14 +1,8 @@
-perguntas = ['Que animal é maior que 3 e menor que 4?', 
-             'Que animal é considerado o rei da floresta? ',
-             'Que animal é conhecido por ter listras pretas e brancas?',
-             'Que animal é conhecido por ser amigo do home?']
-respostas = ["piolho", 'leao', 'zebra', 'cachorro']
+respostas = ['piolho']
 print('''
 Bem vindo!
 Preparamos uma pergunta para você.
+Que animal é maior que 3 e menor que 4?
 ''')
-
-for i, pergunta in enumerate(perguntas):
-    resposta = input(f'{pergunta}').lower().replace("ã", "a").replace('é', 'e').replace('ú', 'u').replace('í','i').replace('ç', 'c').replace('à', 'a').replace('â', 'a')
-    print("Acertou!") if resposta == respostas[i] else print("Errou!")
-
+resposta = input().lower()
+print("Acertou!") if resposta in respostas else print("Errou!")
--- Aula12_exercicio2.py
+++ Aula12_exercicio2.py
@@ -1,13 +1,14 @@
 perguntas = ['Que animal é maior que 3 e menor que 4?', 
              'Que animal é considerado o rei da floresta? ',
-             'Que animal é conhecido por ter listras pretas e brancas?']
-respostas = ["piolho", 'leao', 'zebra']
+             'Que animal é conhecido por ter listras pretas e brancas?',
+             'Que animal é conhecido por ser amigo do home?']
+respostas = ["piolho", 'leao', 'zebra', 'cachorro']
 print('''
 Bem vindo!
 Preparamos uma pergunta para você.
 ''')
 
 for i, pergunta in enumerate(perguntas):
-    resposta = input(f'{pergunta}').lower().replace("ã", "a").replace('é', 'e').replace('ú', 'u').replace('í','i').replace('ç', 'c')
+    resposta = input(f'{pergunta}').lower().replace("ã", "a").replace('é', 'e').replace('ú', 'u').replace('í','i').replace('ç', 'c').replace('à', 'a').replace('â', 'a')
     print("Acertou!") if resposta == respostas[i] else print("Errou!")
 

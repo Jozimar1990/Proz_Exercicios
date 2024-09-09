462604ba00ab3bd38825e779b889288d37a04d43
Jozimar1990 <jozimar.1990@gmail.com>
2024-09-09 19:23:05 -0400

    main.py criado

---
+++ console_jogos/main.py
@@ -0,0 +1,10 @@
+from advinhacao import advinhacao
+from jokenpo import jokenpo
+from quiz import quiz
+
+print(
+    '''
+    Bem vindos ao console de jogos em Python!
+    
+    '''
+)
--- console_jogos/quiz.py
+++ console_jogos/quiz.py
@@ -1,13 +1,14 @@
-perguntas = ['Que animal é maior que 3 e menor que 4?', 
-             'Que animal é considerado o rei da floresta? ',
-             'Que animal é conhecido por ter listras pretas e brancas?',
-             'Que animal é conhecido por ser amigo do home?']
-respostas = ["piolho", 'leao', 'zebra', 'cachorro']
-print('''
-Bem vindo!
-Preparamos uma pergunta para você.
-''')
+def quiz():
+    perguntas = ['Que animal é maior que 3 e menor que 4?', 
+                'Que animal é considerado o rei da floresta? ',
+                'Que animal é conhecido por ter listras pretas e brancas?',
+                'Que animal é conhecido por ser amigo do home?']
+    respostas = ["piolho", 'leao', 'zebra', 'cachorro']
+    print('''
+    Bem vindo!
+    Preparamos uma pergunta para você.
+    ''')
 
-for i, pergunta in enumerate(perguntas):
-    resposta = input(f'{pergunta}').lower().replace("ã", "a").replace('é', 'e').replace('ú', 'u').replace('í','i').replace('ç', 'c').replace('à', 'a').replace('â', 'a')
-    print("Acertou!") if resposta == respostas[i] else print("Errou!")
+    for i, pergunta in enumerate(perguntas):
+        resposta = input(f'{pergunta}').lower().replace("ã", "a").replace('é', 'e').replace('ú', 'u').replace('í','i').replace('ç', 'c').replace('à', 'a').replace('â', 'a')
+        print("Acertou!") if resposta == respostas[i] else print("Errou!")
